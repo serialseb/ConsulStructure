f3970079e51d965039865821d06c9fa1e2ce438c
Sebastien Lambla <seb@serialseb.com>
2017-02-23 19:33:00 +0100

    Simpler JSON parsing

--- src/ConsulStructure/Options/Structure.Converters.cs
+++ src/ConsulStructure/Options/Structure.Converters.cs
@@ -13,13 +13,18 @@ namespace ConsulStructure
             public Func<byte[], string> String { get; set; } = bytes => Encoding.UTF8.GetString(bytes);
             public Func<byte[], int> Int32 { get; set; } = bytes => int.Parse(Encoding.UTF8.GetString(bytes));
             public Func<byte[], bool> Bool { get; set; } = bytes => bool.Parse(Encoding.UTF8.GetString(bytes));
-
             public Func<string, IEnumerable<KeyValuePair<string, byte[]>>> KeyParser { get; set; } = ParseJson;
 
             static IEnumerable<KeyValuePair<string, byte[]>> ParseJson(string content)
             {
-                return SimpleJson.DeserializeObject<KV[]>(content)
-                    .Select(kv => new KeyValuePair<string, byte[]>(kv.Key, Convert.FromBase64String(kv.Value)));
+                var jsonObject = (JsonArray)SimpleJson.DeserializeObject(content);
+                foreach (var obj in jsonObject)
+                {
+                    var item = (JsonObject)obj;
+                    var key = item["Key"].ToString();
+                    var value = Convert.FromBase64String(item["Value"].ToString());
+                    yield return new KeyValuePair<string, byte[]>(key, value);
+                }
             }
         }
     }
