cd04a2ad5ef27c9a1a82e210d07957a5d82d3d05
Sebastien Lambla <seb@serialseb.com>
2017-02-26 23:39:11 +0100

    Extracting ps files

---
+++ build/Build-Solution.ps1
@@ -0,0 +1,18 @@
+
+$Solution = "$($env:APPVEYOR_BUILD_FOLDER)/src/ConsulStructure.sln"
+
+$buildCmd = "C:\Program Files (x86)\MSBuild\14.0\bin\msbuild.exe"
+$buildArgs = @(
+    $Solution,
+    "/l:C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll",
+    "/m",
+    "/p:UseSharedCompilation=false",
+    "/p:Configuration=$env:CONFIGURATION",
+    "/p:Platform=$env:PLATFORM")
+if ($env:APPVEYOR_REPO_TAG -eq $false) {
+  & $buildCmd $buildArgs
+  return
+} else {
+  "Building project with Coverity Scan..."
+  cov-build --dir cov-int $buildCmd $buildArgs;
+}
