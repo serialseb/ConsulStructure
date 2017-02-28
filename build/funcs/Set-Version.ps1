a189b00ea8836da2d397cfa68d3134b5e78d9d7e
Sebastien Lambla <seb@serialseb.com>
2017-02-28 13:21:43 +0100

    Finxing build, again...

--- build/funcs/Set-Version.ps1
+++ build/funcs/Set-Version.ps1
@@ -4,7 +4,7 @@ $build = $env:APPVEYOR_BUILD_NUMBER | % PadLeft 4 '0'
 $major = $version.Major
 $branch = $env:APPVEYOR_REPO_BRANCH
 
-if ($env:APPVEYOR_REPO_TAG){
+if ($env:APPVEYOR_REPO_TAG) {
     $version = $env:APPVEYOR_REPO_TAG_NAME
 } else {
     $buildsForBranch = "/api/projects/$env:APPVEYOR_ACCOUNT_NAME/$env:APPVEYOR_PROJECT_SLUG/history?recordsNumber=4000&branch=$branch"
@@ -22,4 +22,4 @@ if ($env:APPVEYOR_REPO_TAG){
 Set-AppveyorBuildVariable -Name "AssemblyMajor" -Value "$major"
 Update-AppVeyorBuild -Version "$version"
 
-bin/chandler push
+ruby bin/chandler push
