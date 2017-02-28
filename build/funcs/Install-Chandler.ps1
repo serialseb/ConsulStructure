94edf715e49a58ac53bbcd3c9cccd4ce91abf5d8
Sebastien Lambla <seb@serialseb.com>
2017-02-28 13:21:43 +0100

    Chandler fixes

--- build/funcs/Install-Chandler.ps1
+++ build/funcs/Install-Chandler.ps1
@@ -1,4 +1,6 @@
-if (-not (test-path "$env:APPVEYOR_BUILD_FOLDER/Gemfile")) {
-    "gem 'chandler'" > "$env:APPVEYOR_BUILD_FOLDER/Gemfile"
+$gemfile = "$env:APPVEYOR_BUILD_FOLDER/Gemfile"
+if (-not (test-path $gemfile)) {
+    add-content -encoding ascii -path $gemfile -value "gem 'chandler'"
+    add-content -encoding ascii -path $gemfile -value "source 'https://rubygems.org'"
 }
 bundle install --path vendor/ --binstubs
