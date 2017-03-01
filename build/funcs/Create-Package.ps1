8d1318002b19d2d5bb79c24af1e44b2a6e37c8ed
Sebastien Lambla <seb@serialseb.com>
2017-03-01 01:59:53 +0100

    fixup! Chandler fixes

--- build/funcs/Create-Package.ps1
+++ build/funcs/Create-Package.ps1
@@ -3,7 +3,7 @@ $repo = $env:APPVEYOR_REPO_NAME
 function Get-GitHubRepo([string]$uri="") {
     if ($uri -ne "" -and -not $uri.StartsWith("/")) { $uri = "/$uri" }
     $finalUri = "https://api.github.com/repos/$repo$uri?access_token=$($env:GITHUB_TOKEN)"
-    echo "Issuing request to $finalUri"
+    write-host "Issuing request to $finalUri"
     Invoke-WebRequest -Uri $finalUri | ConvertFrom-Json
 }
 
@@ -12,21 +12,21 @@ $releaseNotes = 'Pre-release, see https://github.com/serialseb/ConsulStructure/b
 $nuspecPath = "$($env:APPVEYOR_BUILD_FOLDER)\$($env:SEB_PROJECT_NAME).nuspec"
 $nuspec = [xml](Get-Content $nuspecPath)
 
-if ($env:APPVEYOR_REPO_TAG) {
-    $releaseNotes = Get-GitHubRepo("releases/tags/$($env:APPVEYOR_REPO_TAG_NAME)").body.Replace('"','\"').Replace('### ', '')
+if ($env:APPVEYOR_REPO_TAG -eq $true) {
+    $releaseNotes = (Get-GitHubRepo "releases/tags/$($env:APPVEYOR_REPO_TAG_NAME)").body.Replace('"','\"').Replace('### ', '')
     if ($releaseNotes){ 
         $nuspec.package.metadata.releaseNotes = $releaseNotes
     }
 }
-echo "Release notes: $releaseNotes"
+write-host "Release notes: $releaseNotes"
 
 $repoInfo = Get-GitHubRepo
-echo "Switching license to $env:APPVEYOR_REPO_COMMIT"
+write-host "Switching license to $env:APPVEYOR_REPO_COMMIT"
 if ($repoInfo.description) {
     $nuspec.package.metadata.description = $repoInfo.description
 }
 $nuspec.package.metadata.licenseUrl = "https://github.com/$($env:APPVEYOR_REPO_NAME)/tree/$env:APPVEYOR_REPO_COMMIT/LICENSE.md"
 $nuspec.Save($nuspecPath)
-echo "Saved NuSpec at $nuspecPath"
+write-host "Saved NuSpec at $nuspecPath"
 nuget pack $nuspecPath.nuspec -version $env:APPVEYOR_BUILD_VERSION -basepath $env:APPVEYOR_BUILD_FOLDER/src/$env:SEB_PROJECT_NAME/
 Push-AppveyorArtifact *.nupkg
