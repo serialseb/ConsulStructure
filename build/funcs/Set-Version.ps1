03dfc1316577c2a06562961f61595e2ec411bc12
Sebastien Lambla <seb@serialseb.com>
2017-03-03 10:09:10 +0100

    Remove http connection

--- build/funcs/Set-Version.ps1
+++ build/funcs/Set-Version.ps1
@@ -1,30 +1,31 @@
-$version = [version]$(cat VERSION)
-$baseVersion = $version
-$build = $env:APPVEYOR_BUILD_NUMBER | % PadLeft 4 '0'
+$baseVersion = $(cat VERSION)
+$version = [version]$baseVersion
+$build = $env:APPVEYOR_BUILD_NUMBER
 $major = $version.Major
 $branch = $env:APPVEYOR_REPO_BRANCH
 
-if ($env:APPVEYOR_REPO_TAG) {
+if ($env:APPVEYOR_REPO_TAG -eq $true) {
     $version = $env:APPVEYOR_REPO_TAG_NAME
     $buildVersionPrefix = $version
 } else {
-    $buildsForBranch = "/api/projects/$env:APPVEYOR_ACCOUNT_NAME/$env:APPVEYOR_PROJECT_SLUG/history?recordsNumber=4000&branch=$branch"
-    $lastBuild = (Invoke-WebRequest -Uri $buildsForBranch) | ConvertFrom-Json
-    $lastBuildVersion = $lastBuild.build.version
+    # $buildsForBranch = "/api/projects/$env:APPVEYOR_ACCOUNT_NAME/$env:APPVEYOR_PROJECT_SLUG/history?recordsNumber=4000&branch=$branch"
+    # $lastBuild = (Invoke-WebRequest -Uri $buildsForBranch) | ConvertFrom-Json
+    # $lastBuildVersion = $lastBuild.build.version
 
     if ($branch -eq 'master') {
-        $buildVersionPrefix = "$version-ci"
-        $version = "$buildVersionPrefix-$build"
+        $buildVersionPrefix = "$baseVersion-ci"
     }
     else {
-        $buildVersionPrefix = "$version-b"
-        $version = "$buildVersionPrefix-$branch-$build"
+        $buildVersionPrefix = "$baseVersion-b-$branch"
     }
+
+    $version = "$buildVersionPrefix+$build"
 }
+
+$env:SEB_VERSION_PREFIX = $buildVersionPrefix
+Write-Host "Version '$version', base '$baseVersion', prefix '$env:SEB_VERSION_PREFIX"
 
 Set-AppveyorBuildVariable -Name "AssemblyMajor" -Value "$major"
 Update-AppVeyorBuild -Version "$version"
 
-$env:SEB_VERSION_PREFIX = $buildVersionPrefix
-Write-Host "Version '{$version}' with prefix {$env:SEB_VERSION_PREFIX}"
 ruby bin/chandler push
