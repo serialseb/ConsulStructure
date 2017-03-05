fecd04d7f9d14186c0444af76d8797da409ee859
Sebastien Lambla <seb@serialseb.com>
2017-03-05 15:38:50 +0100

    branch sent to sonarqube

--- build/funcs/Begin-SonarQube.ps1
+++ build/funcs/Begin-SonarQube.ps1
@@ -1,2 +1,13 @@
 $sonarBuildId = "com:github:$($env:APPVEYOR_REPO_NAME.Replace("/", ":"))"
-MSBuild.SonarQube.Runner.exe begin /k:"$sonarBuildId" /n:"$env:SEB_PROJECT_NAME" /v:$env:SEB_VERSION_BASE /d:sonar.cs.opencover.reportsPaths="opencoverCoverage.xml" /d:sonar.cs.xunit.reportsPaths=="XUnitResults.xml" /d:"sonar.host.url=https://sonarqube.com" /d:"sonar.login=$env:SONARQUBE_TOKEN"
+$branchParam = ""
+
+if ($env:APPVEYOR_REPO_TAG -eq $true) { $branchParam = "/d:sonar.branch=$env:APPVEYOR_REPO_BRANCH"}
+
+MSBuild.SonarQube.Runner.exe begin `
+    /k:"$sonarBuildId" `
+    /n:"$env:SEB_PROJECT_NAME" `
+    /v:$env:SEB_VERSION_BASE `
+    /d:sonar.cs.opencover.reportsPaths="opencoverCoverage.xml" `
+    /d:sonar.cs.xunit.reportsPaths=="XUnitResults.xml" `
+    /d:"sonar.host.url=https://sonarqube.com" `
+    /d:"sonar.login=$env:SONARQUBE_TOKEN" $branchParam
